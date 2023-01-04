7e14933d521e670bc30515c46fbb5a4bb2709cdc
Greg Cain <codegregcode@gmail.com>
2023-01-04 17:06:55 +0000

    updates pet following code review

--- src/pet.js
+++ src/pet.js
@@ -1,3 +1,11 @@
+const ADD_HUNGER = 5;
+const TAKE_FITNESS = 3;
+const MAXIMUM_FITNESS = 10;
+const ADD_FITNESS = 4;
+const MIN_HUNGER = 0;
+const TAKE_HUNGER = 3;
+const FIT_LEVEL = 3;
+const HUNGER_LEVEL = 5;
 class Pet {
     constructor(name) {
         this.name = name;
@@ -13,8 +21,6 @@ class Pet {
         if(!this.isAlive) {
             throw new Error('Your pet is no longer alive :(')
         }
-        const ADD_HUNGER = 5;
-        const TAKE_FITNESS = 3;
         this.age += 1;
         this.hunger += ADD_HUNGER;
         this.fitness -= TAKE_FITNESS;
@@ -23,24 +29,18 @@ class Pet {
         if(!this.isAlive) {
             throw new Error('Your pet is no longer alive :(')
         }
-        const MAXIMUM_FITNESS = 10;
-        const ADD_FITNESS = 4;
-        (this.fitness + ADD_FITNESS) <= MAXIMUM_FITNESS ? this.fitness += ADD_FITNESS : this.fitness = MAXIMUM_FITNESS;
+        this.fitness = Math.min(this.fitness + ADD_FITNESS, MAXIMUM_FITNESS)
     }
     feed() {
         if(!this.isAlive) {
             throw new Error('Your pet is no longer alive :(')
         }
-        const MIN_HUNGER = 0;
-        const TAKE_HUNGER = 3;
-        (this.hunger - TAKE_HUNGER) >= MIN_HUNGER ? this.hunger -= TAKE_HUNGER : this.hunger = MIN_HUNGER;
+        this.hunger = Math.max(this.hunger - TAKE_HUNGER, MIN_HUNGER);
         }
     checkUp() {
         if(!this.isAlive) {
             throw new Error('Your pet is no longer alive :(')
         }
-        const FIT_LEVEL = 3;
-        const HUNGER_LEVEL = 5;
         if (this.fitness <= FIT_LEVEL && this.hunger >= HUNGER_LEVEL) {
             return `I am hungry AND I need a walk`;
         } else if (this.hunger >= HUNGER_LEVEL) {
